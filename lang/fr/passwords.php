ce2d2cb31f4833af3f806343c494e852cfb4d40a
Alain Avoce <avocealain1@gmail.com>
2026-07-14 17:53:45 +0300

    Les messages en fr

---
+++ lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+    //afficher ces messages en fr
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous avons envoyé le lien de réinitialisation de votre mot de passe.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce token de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne pouvons pas trouver d'utilisateur avec cette adresse e-mail.",
+
+];
